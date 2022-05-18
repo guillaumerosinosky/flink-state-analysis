7f85c77f30c549736342028d1d142f0a099874c5
Guillaume Rosinosky <guillaumerosinosky@yahoo.fr>
2022-05-18 19:53:38 +0200

    switch to Mb

--- scripts/data_generator.py
+++ scripts/data_generator.py
@@ -18,7 +18,7 @@ def main():
     tables = 2
     keys = 10
     key_matching = 0.5
-    dataset_size = 1 # In Gigabytes
+    dataset_size = 1024 # In Megabytes
     row_size = 1024
     data_dir = "/home/donatien/GEPICIAD/resource-estimator/xp/sql_illustration/data/tables/"
 
@@ -30,7 +30,7 @@ def main():
         elif o in ("-m", "--key_matching"):
             key_matching = float(a)
         elif o in ("-s", "--size"):
-            dataset_size = float(a)
+            dataset_size = int(a)
         elif o in ("-r", "--row_size"):
             row_size = int(a)
         else:
@@ -38,13 +38,14 @@ def main():
 
     pathlib.Path(data_dir).mkdir(exist_ok=True, parents=True)
     table_row = "{}|{}\n"
-    size_per_table = dataset_size * 1073741824 # in bytes
+    size_per_table = dataset_size * 1048576  # in bytes
     rows_per_table = int(size_per_table / (row_size + 3)) # Row size + 3 bytes for key (1), separator (1) and EOL (1)
-
     open(data_dir + "table_1.dat", "w").write(''.join(table_row.format(i, 'A'*10) for i in range(keys)))
     with open(data_dir + "table_2.dat", "w") as fd:
-        #print("Writing {}/{}Gb".format(i+1, dataset_size))
-        fd.write(''.join(table_row.format(randint(0, int(keys / key_matching)), 'B'*(row_size)) for j in range(int(rows_per_table / dataset_size))))
+        for i in range(dataset_size):
+            print("",  end="\r")
+            print("Writing {}/{}Mb".format(i+1, dataset_size), end=" ")
+            fd.write(''.join(table_row.format(randint(0, int(keys / key_matching)), 'B'*(row_size)) for j in range(int(rows_per_table / dataset_size))))
 
 if __name__ == "__main__":
     main()
